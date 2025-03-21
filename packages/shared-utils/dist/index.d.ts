8d8bd8e7ad786fba37a997ee765b3ed12f3d70e8
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-03-21 16:10:00 -0700

    init

---
+++ packages/shared-utils/dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function formatTemperature(temp: number): string;
+export declare function formatHumidity(humidity: number): string;
+export declare function formatWindSpeed(speed: number): string;
---
+++ packages/shared-utils/dist/index.js
@@ -0,0 +1,9 @@
+export function formatTemperature(temp) {
+    return `${temp.toFixed(1)}°C`;
+}
+export function formatHumidity(humidity) {
+    return `${humidity}%`;
+}
+export function formatWindSpeed(speed) {
+    return `${speed.toFixed(1)} km/h`;
+}
